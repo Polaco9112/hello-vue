d684d85fdb7b14dec22ddf25696048cab42b5c7f
cc-cesar-tobon <cesar.tobon@finoservices.com>
2024-10-22 14:38:42 -0500

    Cambios en el componente

--- src/components/ExerciseComputed.vue
+++ src/components/ExerciseComputed.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
     <h1 class="title">Calculadora de Impuestos</h1>
+    
+    <form class="calc">
+      <label>Subtotal</label>
+      <input type="number" v-model="subtotal">
+      <h3>Total de Impuestos: {{ calculateTaxes }} ({{ impuesto }}%)</h3>
+    </form>
+    
   </div>
 </template>
 
 <script lang='ts' setup>
+  import {Ref, ref, computed} from 'vue'
 
+  const impuesto = 19
+
+  let subtotal: Ref<number> = ref(0)
+
+  const calculateTaxes = computed(() => (impuesto * subtotal.value) / 100 )
 </script>
 
 <style scoped>
@@ -13,5 +26,9 @@
     text-align: center;
     font-size: 20px;
     font-weight: bold;
+  }
+  .calc{
+    justify-content: center;
+    align-items: center;
   }
 </style>
